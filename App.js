d5a0642df297c1baff847b6f3d1f7353ce64e2fb
Chris <chris.demetriad@thecommercepartnership.com>
2020-10-15 00:03:07 +0100

    Adding the react-native-dotenv lib and securing the API key

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState, Button } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
+import { API_KEY } from "@env";
 
 export default function App() {
 	const [data, setData] = useState({});
 
 	const url = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles";
-	const API_KEY = "nice one :)";
 
 	const load = async () => {
 		const response = await fetch(url, {
@@ -31,8 +31,8 @@ export default function App() {
 
 	return (
 		<View style={styles.container}>
-			<StatusBar style="auto" />
 			<TextInput style={styles.input} value="BA65 PDQ" />
+			<StatusBar style="auto" />
 		</View>
 	);
 }
@@ -40,9 +40,28 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: "#ffffff",
+		backgroundColor: "#1a2a57",
 		alignItems: "center",
+		color: "#ffffff",
 		justifyContent: "center",
+		padding: 20,
 	},
-	input: {},
+	input: {
+		borderColor: "green",
+		borderWidth: 10,
+		paddingHorizontal: 40,
+		paddingVertical: 4,
+		borderRadius: 10,
+		backgroundColor: "yellow",
+		fontSize: 35,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+
+		elevation: 5,
+	},
 });
