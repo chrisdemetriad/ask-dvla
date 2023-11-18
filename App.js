f93c38c314592c699ecc72a3fd50a8c9e8138d61
Chris Demetriad <chris@demetriad.co.uk>
2023-11-18 12:57:29 +0000

    Extracting vehicle details in its own component

--- App.js
+++ App.js
@@ -55,48 +55,7 @@ export default function App() {
 					</View>
 					<TextInput onSubmitEditing={onPress} autoCapitalize="characters" spellCheck={false} autoCorrect={false} textAlign={"center"} onChangeText={onChangeText} style={styles.input} placeholder="BA65 PDQ" />
 				</View>
-				{data.make && (
-					<View style={styles.results}>
-						<Text style={styles.overview}>
-							{yearOfManufacture} {make}, {colour}, {engineCapacity}cc, {fuelType}
-							{revenueWeight && ", " + revenueWeight + "kg"}
-						</Text>
-						<Line />
-						<View style={styles.resultsContainer}>
-							<View style={styles.common}>
-								{motStatus == "Not valid" ? (
-									<View>
-										<AntDesign name="warning" size={34} color="red" />
-										<Text style={[styles.status, styles.statusError]}>NOT VALID</Text>
-									</View>
-								) : (
-									<View>
-										<MaterialIcons name="check" size={34} color="green" />
-										<Text style={styles.status}>{motStatus}</Text>
-									</View>
-								)}
-								<Text style={styles.title}>Mot</Text>
-								<Text style={styles.date}>{motExpiryDate}</Text>
-							</View>
-							<View style={(styles.tax, styles.common)}>
-								{taxStatus == "SORN" ? (
-									<View>
-										<AntDesign name="warning" size={34} color="red" />
-										<Text style={[styles.status, styles.statusError]}>SORN</Text>
-									</View>
-								) : (
-									<View>
-										<MaterialIcons name="check" size={34} color="green" />
-										<Text style={styles.status}>{taxStatus}</Text>
-									</View>
-								)}
-								<Text style={styles.title}>Tax</Text>
-								<Text style={styles.date}>{taxDueDate}</Text>
-							</View>
-						</View>
-					</View>
-				)}
-				{!data.make && <Text style={styles.error}>Please enter a valid registration plate number.</Text>}
+				{data.make ? <VehicleDetails vehicleData={data} /> : <Text style={styles.error}>Please enter a valid registration plate number.</Text>}
 				<StatusBar style="light" />
 			</View>
 		);
