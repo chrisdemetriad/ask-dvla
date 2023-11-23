02d6fbaecbead052d642ad9b78018f4c41104eba
Chris Demetriad <chris@demetriad.co.uk>
2023-11-23 13:16:23 +0000

    Hiding top headers on stack navigator

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -13,24 +13,24 @@ const BottomTabNavigator = () => {
 			screenOptions={({ route }) => ({
 				tabBarIcon: ({ focused, color, size }) => {
 					let iconName;
-					if (route.name === "Home") {
+					if (route.name === "HOME") {
 						iconName = "home";
-					} else if (route.name === "Help") {
+					} else if (route.name === "HELP") {
 						iconName = "help";
-					} else if (route.name === "Settings") {
+					} else if (route.name === "SETTINGS") {
 						iconName = "settings";
 					}
 					return <Icon name={iconName} size={focused ? 30 : 25} color={color} />;
 				},
 				tabBarActiveTintColor: Platform.OS === "ios" ? "#007aff" : "#333",
 				tabBarInactiveTintColor: "gray",
-				tabBarLabelStyle: { paddingBottom: Platform.OS === "ios" ? 5 : 0 },
-				tabBarStyle: { height: 60, paddingVertical: Platform.OS === "ios" ? 10 : 0 },
+				tabBarLabelStyle: { paddingBottom: Platform.OS === "ios" ? 5 : 2 },
+				tabBarStyle: { height: 60, paddingVertical: Platform.OS === "ios" ? 10 : 5, paddingBottom: Platform.OS === "ios" ? 10 : 10 },
 			})}
 		>
-			<Tab.Screen name="Home" component={HomeScreen} />
-			<Tab.Screen name="Help" component={HelpScreen} />
-			<Tab.Screen name="Settings" component={SettingsScreen} />
+			<Tab.Screen name="HOME" component={HomeScreen} options={{ headerShown: false }} />
+			<Tab.Screen name="HELP" component={HelpScreen} options={{ headerShown: false }} />
+			<Tab.Screen name="SETTINGS" component={SettingsScreen} options={{ headerShown: false }} />
 		</Tab.Navigator>
 	);
 };
