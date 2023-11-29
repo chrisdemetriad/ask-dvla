cc7c6f2b47e4174f5acc16578de2f715ccbb4292
Chris Demetriad <chris@demetriad.co.uk>
2023-11-29 09:37:11 +0000

    Fixing import path as Jest didn't like it

--- hooks/useVehicleData.ts
+++ hooks/useVehicleData.ts
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import { API_URL, API_TOKEN } from "@env";
+import { API_URL, API_TOKEN } from "react-native-dotenv";
 import vehicleDataReducer from "../reducers/vehicleDataReducer";
 
 interface VehicleDataState {
