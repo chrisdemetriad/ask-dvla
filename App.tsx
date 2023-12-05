aec06fcf925e601e7e1ab6d2151e921bc0d538fe
Chris Demetriad <chris@demetriad.co.uk>
2023-12-05 11:57:41 +0000

    Sorting out minor issue

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import CustomStatusBar from "./components/CustomStatusBar";
 
 const App: React.FC = () => {
 	return (
-		<View testID="ask-dvla-app">
+		<View style={styles.container} testID="ask-dvla-app">
 			<NavigationContainer>
 				<CustomStatusBar />
 				<BottomTabNavigator />
@@ -14,5 +14,11 @@ const App: React.FC = () => {
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
 
 export default App;
