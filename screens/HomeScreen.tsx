636c98104b79a6a08efdbaf0ba1d08dcf3bbb8bf
Chris Demetriad <chris@demetriad.co.uk>
2023-12-12 23:39:47 +0000

    Fixing homepage so styling is consistent with the rest of the app

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
-import appStyles from "./../App.style.js";
+import { Text, View, ScrollView, TouchableOpacity, StyleSheet, ActivityIndicator } from "react-native";
 import { useFonts } from "expo-font";
 import VehicleDetails from "../components/VehicleDetails";
 import useVehicleData from "../hooks/useVehicleData";
 import SearchBox from "../components/SearchBox";
 import { useTheme } from "./../ThemeContext";
-import getStyles from "./../App.style.js";
+import getStyles from "./../App.style";
 
 const HomeScreen: React.FC = () => {
 	const [number, setNumber] = useState("");
@@ -21,6 +20,9 @@ const HomeScreen: React.FC = () => {
 	});
 
 	const styles = StyleSheet.create({
+		screen: {
+			flex: 1,
+		},
 		info: {
 			fontFamily: "RobotoCondensed_300Light",
 			fontSize: 20,
@@ -40,25 +42,39 @@ const HomeScreen: React.FC = () => {
 			justifyContent: "center",
 			alignItems: "center",
 		},
+		title: {
+			fontSize: 24,
+			fontWeight: "bold",
+			padding: 15,
+			color: isDarkTheme ? "#eee" : "#333",
+		},
+		centered: {
+			flex: 1,
+		},
 	});
 
 	if (isLoading || !fontsLoaded) {
 		return (
-			<View testID="home-screen" style={[styles.container, appStyles.centeredContainer]} accessibilityLabel="Loading">
+			<View testID="check-mot-tax" style={[styles.screen, appStyles.container]} accessibilityLabel="Loading">
 				<ActivityIndicator size="large" color="#0076bc" />
 			</View>
 		);
 	} else {
 		return (
-			<View testID="home-screen" style={[styles.container, appStyles.centeredContainer, appStyles.container]} accessibilityLabel="Home screen">
-				{!data.make && (
-					<View>
-						<Text style={styles.info}>Please enter a valid registration plate number</Text>
-					</View>
-				)}
-				<SearchBox fetchVehicleData={fetchVehicleData} number={number} setNumber={setNumber} />
-				{data.make && !error && <VehicleDetails vehicleData={data} />}
-				{error && <Text style={[styles.info, styles.error]}>{error}</Text>}
+			<View testID="check-mot-tax" style={[styles.screen, appStyles.container]} accessibilityLabel="Check MOT and Vehicle Tax">
+				<Text style={styles.title}>Check MOT and Vehicle Tax</Text>
+				{/* <ScrollView style={styles.centered}> */}
+				<View style={[styles.container, appStyles.centeredContainer, appStyles.container]}>
+					{!data.make && (
+						<View>
+							<Text style={styles.info}>Please enter a valid registration plate number</Text>
+						</View>
+					)}
+					<SearchBox fetchVehicleData={fetchVehicleData} number={number} setNumber={setNumber} />
+					{data.make && !error && <VehicleDetails vehicleData={data} />}
+					{error && <Text style={[styles.info, styles.error]}>{error}</Text>}
+				</View>
+				{/* </ScrollView> */}
 			</View>
 		);
 	}
